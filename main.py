1fa54687d5b91416e80562cb1056529bbc50bca7
Ameya <ameyamorgaonkar@gmail.com>
2025-10-17 19:23:34 +0530

    Opened WebCam to Process the live image

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import time
 import cv2
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+while True:
+    check, frame = video.read()
+
+    cv2.imshow("Ameya's Camera", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
