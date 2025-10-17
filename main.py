f77d18f9d23283cede902c72b322a5ecc40c2f8f
Ameya <ameyamorgaonkar@gmail.com>
2025-10-17 19:27:40 +0530

    minor changes

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import time
 import cv2
 
 video = cv2.VideoCapture(0)
-time.sleep(1)
+# time.sleep(1)
 
 while True:
     check, frame = video.read()
+    print(check)
 
-    cv2.imshow("Ameya's Camera", frame)
+    cv2.imshow("Camera", frame)
 
     key = cv2.waitKey(1)
 
